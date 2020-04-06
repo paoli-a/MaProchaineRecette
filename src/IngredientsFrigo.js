64d4470bfbbfb4a095fc374b44057957d40f1e40
Paoli <aude.paoli910@gmail.com>
2020-04-06 11:15:10 +0200

    feat: Ajout des fonctionnalités de suppression et d'ajout d'un ingredient frigo

--- src/IngredientsFrigo.js
+++ src/IngredientsFrigo.js
@@ -1,14 +1,68 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 function IngredientsFrigo ({ingredients}) {
 
-  const ingredientElement = ingredients.map((monIngredient) => {
+  const [ingredientsList, setIngredient] = useState(ingredients);
+  const [ingredientName, setIngredientName] = useState("");
+  const [ingredientQuantite, setIngredientQuantite] = useState("");
+  const [quantiteUnite, setQuantiteUnité] = useState("g");
+  const [ingredientPeremption, setIngredientPeremption] = useState("");
+
+
+  const handleSupprClick = (id) => {
+    supprIngredient(id);
+  }
+
+  const supprIngredient = (id) => {
+    const ingredientsListUpdated = ingredientsList.slice()
+    const index = ingredientsListUpdated.findIndex((ingredient) => {
+      return ingredient.id === id
+    });
+    ingredientsListUpdated.splice(index,1);
+    setIngredient(ingredientsListUpdated)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const id = new Date().getTime();
+    const quantite = ingredientQuantite + quantiteUnite;
+    const datePeremption = new Date(ingredientPeremption);
+
+    const ingredientNouveau = {id : id, nom : ingredientName,
+      datePeremption : datePeremption, quantite : quantite};
+
+    const ingredientsListUpdated = ingredientsList.slice();
+    ingredientsListUpdated.push(ingredientNouveau);
+    setIngredient(ingredientsListUpdated)
+  }
+
+  const handleChangeName = (event) => {
+    const valueName = event.currentTarget.value;
+    setIngredientName(valueName)
+  }
+
+  const handleChangeQuantite = (event) => {
+    const valueQuantite = event.currentTarget.value;
+    setIngredientQuantite(valueQuantite)
+  }
+
+  const handleChangePeremption = (event) => {
+    const valuePeremption = event.currentTarget.value;
+    setIngredientPeremption(valuePeremption)
+  }
+
+  const handleChangeUnite = (event) => {
+    const valueUnite = event.currentTarget.value;
+    setQuantiteUnité(valueUnite)
+  }
+
+  const ingredientElement = ingredientsList.map((monIngredient) => {
     const formatedDate = monIngredient.datePeremption.toLocaleDateString()
     return (
       <li key={monIngredient.id}>
         {monIngredient.nom} : {monIngredient.quantite}. Expiration : {formatedDate}.
-        <button>Supprimer</button>
+        <button onClick={() => handleSupprClick(monIngredient.id)}>Supprimer</button>
       </li>
     )
   })
@@ -16,13 +70,32 @@ function IngredientsFrigo ({ingredients}) {
   return(
     <div>
       <h1> Voici les ingrédients du frigo !</h1>
+      <form onSubmit = {handleSubmit}>
+        Ajouter un ingredient
+        <div>
+          <label htmlFor="nomIngredient">Nom de l'ingrédient : </label>
+          <input type="text" id="nomIngredient" value={ingredientName}
+            onChange={handleChangeName}/>
+        </div>
+        <div>
+          <label htmlFor="quantiteIngredient">Quantité : </label>
+          <input type="text" id="quantiteIngredient" value={ingredientQuantite}
+            onChange={handleChangeQuantite}/>
+          <select value={quantiteUnite} onChange={handleChangeUnite}
+            aria-label="Unité">
+            <option value="kg">kg</option>
+            <option value="g">g</option>
+            <option value="cl">cl</option>
+          </select>
+        </div>
+        <label htmlFor="datePeremption">Date de péremption : </label>
+        <input type="date" id="datePeremption" value={ingredientPeremption}
+          onChange={handleChangePeremption}/>
+        <button>Confirmer</button>
+      </form>
       <ul>
         {ingredientElement}
       </ul>
-      <form>
-        <input type="text" placeholder="Ajouter un ingrédient" />
-      <button>Confirmer</button>
-      </form>
     </div>
   )
 }
--- src/IngredientsFrigo.test.js
+++ src/IngredientsFrigo.test.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, within } from '@testing-library/react';
 import IngredientsFrigo from './IngredientsFrigo';
 
 let ingredientsFrigo
@@ -22,27 +22,65 @@ beforeEach(() => {
   ]
 });
 
-it('renders names of ingredients', () => {
-  const { getByText } = render(<IngredientsFrigo ingredients={ingredientsFrigo} />);
-  const ingredient1 = getByText("épinard", { exact: false })
-  expect(ingredient1).toBeInTheDocument();
-});
+describe("correct display of an ingredient", () => {
+  it('renders names of ingredients', () => {
+    const { getByText } = render(<IngredientsFrigo ingredients={ingredientsFrigo} />);
+    const ingredient1 = getByText("épinard", { exact: false })
+    expect(ingredient1).toBeInTheDocument();
+  });
 
-it('renders expiration dates of ingredients', () => {
-  const { getByText } = render(<IngredientsFrigo ingredients={ingredientsFrigo} />);
-  const ingredient2 = getByText("céleri rave", { exact: false })
-  const expectedDate = ingredientsFrigo[1].datePeremption.toLocaleDateString()
-  expect(ingredient2.textContent).toContain(expectedDate);
-});
+  it('renders expiration dates of ingredients', () => {
+    const { getByText } = render(<IngredientsFrigo ingredients={ingredientsFrigo} />);
+    const ingredient2 = getByText("céleri rave", { exact: false })
+    const expectedDate = ingredientsFrigo[1].datePeremption.toLocaleDateString()
+    expect(ingredient2.textContent).toContain(expectedDate);
+  });
 
-it('renders quantities of ingredients', () => {
-  const { getByText } = render(<IngredientsFrigo ingredients={ingredientsFrigo} />);
-  const ingredient1 = getByText("épinard", { exact: false })
-  expect(ingredient1.textContent).toContain("60g");
-});
+  it('renders quantities of ingredients', () => {
+    const { getByText } = render(<IngredientsFrigo ingredients={ingredientsFrigo} />)
+    const ingredient1 = getByText("épinard", { exact: false })
+    expect(ingredient1.textContent).toContain("60g");
+  });
 
-it('renders the right number of ingredients', () => {
-  const { getAllByRole } = render(<IngredientsFrigo ingredients={ingredientsFrigo} />);
-  const listItems = getAllByRole('listitem')
-  expect(listItems).toHaveLength(2)
-});
+  it('renders the right number of ingredients', () => {
+    const { getAllByRole } = render(<IngredientsFrigo ingredients={ingredientsFrigo} />);
+    const listItems = getAllByRole('listitem')
+    expect(listItems).toHaveLength(2)
+  });
+})
+
+describe("functionalities work properly", () => {
+  it('removes the correct ingredient when clicking on remove button', () => {
+    const { getByText, getAllByRole } = render(<IngredientsFrigo ingredients={ingredientsFrigo} />);
+    const ingredient = getByText("épinard", { exact: false });
+    const button = within(ingredient).getByText("Supprimer");
+    fireEvent.click(button);
+    const listItems = getAllByRole('listitem');
+    expect(ingredient).not.toBeInTheDocument();
+    expect(listItems).toHaveLength(1)
+  });
+
+  it('adds the correct ingredient when filling the form and clicking on submit', () => {
+    const { getByLabelText, getByText, getAllByRole } = render(<IngredientsFrigo ingredients={ingredientsFrigo} />);
+    addIngredient(getByLabelText,getByText);
+    const ingredient = getByText("carotte", { exact: false });
+    const listItems = getAllByRole('listitem');
+    const expectedDate = new Date("2020-04-03");
+    expect(listItems).toHaveLength(3);
+    expect(ingredient.textContent).toContain("1kg");
+    expect(ingredient.textContent).toContain(expectedDate.toLocaleDateString())
+  })
+
+  function addIngredient (getByLabelText, getByText) {
+    const inputNom =  getByLabelText("Nom de l'ingrédient :");
+    const inputQuantite = getByLabelText("Quantité :");
+    const inputDate = getByLabelText("Date de péremption :");
+    const selectedUnit = getByLabelText("Unité");
+    const submitButton = getByText("Confirmer");
+    fireEvent.change(inputNom, { target: { value: 'carotte' } });
+    fireEvent.change(inputQuantite, { target: { value: '1' } });
+    fireEvent.change(inputDate, { target: { value: '2020-04-03' } });
+    fireEvent.change(selectedUnit, { target: { value: 'kg' } });
+    fireEvent.click(submitButton);
+  }
+})
