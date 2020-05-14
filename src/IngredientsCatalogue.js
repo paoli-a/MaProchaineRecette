456c4213f97296869623210a6ca2fd91deff3a5f
Paoli <aude.paoli910@gmail.com>
2020-05-14 16:37:15 +0200

    feat: barre de recherche filtrante sur IngredientsCatalogue

--- src/IngredientsCatalogue.js
+++ src/IngredientsCatalogue.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState, useMemo } from 'react';
 import { useForm } from 'react-hook-form';
 
 function IngredientsCatalogue ({ingredientsPossibles}) {
 
   const { register, handleSubmit, errors, reset} = useForm()
   const [ingredientsCatalogue, setIngredientsCatalogue] = useState(ingredientsPossibles);
+  const [searchResults, setSearchResults] = useState("")
 
   const onSubmitWrapper = (data) => {
     const id = new Date().getTime();
@@ -24,19 +25,50 @@ function IngredientsCatalogue ({ingredientsPossibles}) {
     setIngredientsCatalogue(ingredientsListUpdated)
   }
 
+  const handleChangeSearch = (event) => {
+    setSearchResults(event.target.value.toLowerCase())
+  }
 
-  const ingredient = ingredientsCatalogue.map((unIngredient) => {
-    return (
-      <li key={unIngredient.id}>
-        {unIngredient.nom}
-        <button onClick={() => handleSupprClick(unIngredient.id)}>X</button>
-      </li>
-    )
-  })
+  const ingredientsFiltres = useMemo(() => {
+    const filtreurUtilSearch = function(ingredient) {
+      const ingredientCatalogueLower = ingredient.nom.toLowerCase()
+      const totalLetters = Math.min(ingredient.nom.length, searchResults.length)
+      for (let i=0; i<totalLetters; i++) {
+        if(ingredientCatalogueLower[i] !== searchResults[i]) {
+          return false
+        }
+      }
+      return true
+    }
+
+    const filtreurRecettesSearch = function() {
+      if (searchResults === "") {
+        return ingredientsCatalogue
+      }
+      else {
+        return ingredientsCatalogue.filter(filtreurUtilSearch);
+      }
+    }
+
+    return filtreurRecettesSearch()
+  }, [ingredientsCatalogue, searchResults])
+
+  const ingredient = ingredientsFiltres.map(
+    (unIngredient) => {
+      return (
+        <li key={unIngredient.id}>
+          {unIngredient.nom}
+          <button onClick={() => handleSupprClick(unIngredient.id)}>X</button>
+        </li>)
+    })
 
   return(
     <div>
       <h1> Catalogue des ingrédients</h1>
+      <form>
+        <input type="search" id="rechercheCatalogueIngredient" name="q"
+        placeholder="Recherche..." spellCheck="true" size="30" onChange={handleChangeSearch} />
+      </form>
       <form id="ingredientForm" onSubmit={handleSubmit(onSubmitWrapper)} >
         <fieldset>
         <legend>Ajouter un ingredient dans le catalogue</legend>
