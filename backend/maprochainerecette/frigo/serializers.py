b4440ac06d209ca89462cd4c175a0c1ae9ef6217
Paoli <aude.paoli910@gmail.com>
2020-07-27 18:26:17 +0200

    refactor: replace nested serializers by SlugRelatedField

--- backend/maprochainerecette/frigo/serializers.py
+++ backend/maprochainerecette/frigo/serializers.py
@@ -1,5 +1,3 @@
-from django.shortcuts import get_object_or_404
-
 from rest_framework import serializers
 from frigo.models import IngredientFrigo
 from catalogues.models import Ingredient, Unite
@@ -10,12 +8,6 @@ class IngredientFrigoSerializer(serializers.ModelSerializer):
         queryset=Ingredient.objects.all(), slug_field="nom")
     unite = serializers.SlugRelatedField(
         queryset=Unite.objects.all(), slug_field="abbreviation")
-
-    def get_ingredient(self, obj):
-        return obj.ingredient.nom
-
-    def get_unite(self, obj):
-        return obj.unite.abbreviation
 
     class Meta:
         model = IngredientFrigo
