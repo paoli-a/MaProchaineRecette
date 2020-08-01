465e3c3e1f631c249c2e0b1fcf5aa39d6c48b045
Paoli <aude.paoli910@gmail.com>
2020-08-02 00:41:30 +0200

    feat: merge fridge ingredients having the same date, name and unit type together on backend side

--- backend/maprochainerecette/frigo/models.py
+++ backend/maprochainerecette/frigo/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.db import models
 from model_utils.models import TimeStampedModel
 
@@ -13,3 +15,36 @@ class IngredientFrigo(TimeStampedModel):
 
     def __str__(self) -> str:
         return self.ingredient.nom
+
+    def save(self, *args, **kwargs):
+        """Save the fridge ingredient or update the corresponding exsisting ingredient.
+
+        Fridge ingredients having the same catalog ingredient, expiration date and unit type
+        should be merged together, and their value summed.
+
+        """
+        if not self.pk:
+            mergeable = IngredientFrigo.objects.filter(ingredient=self.ingredient,
+                                                       date_peremption=self.date_peremption,
+                                                       unite__type=self.unite.type).first()
+            if mergeable:
+                self._update_mergeable_with_new_ingredient(mergeable)
+            else:
+                super(IngredientFrigo, self).save(*args, **kwargs)
+        else:
+            super(IngredientFrigo, self).save(*args, **kwargs)
+
+    def _update_mergeable_with_new_ingredient(self, mergeable):
+        if mergeable.unite.rapport > self.unite.rapport:
+            quantite = self._compute_quantite(mergeable, self)
+        else:
+            quantite = self._compute_quantite(self, mergeable)
+            mergeable.unite = self.unite
+        mergeable.quantite = quantite
+        mergeable.save()
+
+    @staticmethod
+    def _compute_quantite(big_element, small_element):
+        rapport = Decimal(small_element.unite.rapport) / \
+            Decimal(big_element.unite.rapport)
+        return Decimal(big_element.quantite) + Decimal(small_element.quantite) * rapport
