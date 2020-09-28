3ada5b7cfe83757740e6def1edbf5ab36b015b57
Roman Mkrtchian <mkrtchia@gmail.com>
2020-09-28 22:50:40 +0200

    prepare database before end to end tests

--- frontend/end_to_end/end_to_end_server.js
+++ frontend/end_to_end/end_to_end_server.js
@@ -2,22 +2,33 @@ const execSync = require("child_process").execSync;
 
 module.exports = {
   start: function () {
-    execSync("node_modules/.bin/pm2 start pm2_backend.json", {
-      encoding: "utf-8",
-    });
-    const output = execSync("node_modules/.bin/pm2 start pm2_frontend.json", {
-      encoding: "utf-8",
-    });
-    console.log("Output was:\n", output);
+    execute(
+      "cd ../backend/maprochainerecette && DATABASE_URL=sqlite:///end_to_end_db.sqlite3 pipenv run python manage.py flush --noinput",
+      true
+    );
+    execute(
+      "cd ../backend/maprochainerecette && DATABASE_URL=sqlite:///end_to_end_db.sqlite3 pipenv run python manage.py makemigrations",
+      true
+    );
+    execute(
+      "cd ../backend/maprochainerecette && DATABASE_URL=sqlite:///end_to_end_db.sqlite3 pipenv run python manage.py migrate",
+      true
+    );
+    execute("node_modules/.bin/pm2 start pm2_backend.json", false);
+    execute("node_modules/.bin/pm2 start pm2_frontend.json", true);
   },
 
   stop: function () {
-    execSync("node_modules/.bin/pm2 stop frontend", {
-      encoding: "utf-8",
-    });
-    const output = execSync("node_modules/.bin/pm2 stop backend", {
-      encoding: "utf-8",
-    });
-    console.log("Output was:\n", output);
+    execute("node_modules/.bin/pm2 stop frontend", false);
+    execute("node_modules/.bin/pm2 stop backend", true);
   },
 };
+
+function execute(command, output_result = false) {
+  let output = execSync(command, {
+    encoding: "utf-8",
+  });
+  if (output_result) {
+    console.log("__________\n", output);
+  }
+}
