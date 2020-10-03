f8ddff8e7f6da70c6b1148902d102c9149f84c96
Roman Mkrtchian <mkrtchia@gmail.com>
2020-10-03 18:13:45 +0200

    make UnitViewSet read only again

--- backend/maprochainerecette/units/api.py
+++ backend/maprochainerecette/units/api.py
@@ -3,7 +3,7 @@ from units.models import Unit
 from units.serializers import UnitSerializer
 
 
-class UnitViewSet(viewsets.ModelViewSet):
+class UnitViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Unit.objects.all()
     permission_classes = [permissions.AllowAny]
     authentication_classes = [authentication.TokenAuthentication]
