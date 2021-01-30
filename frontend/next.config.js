433b9435e0146eaa3dd7c52430c7a7e379b2c5f7
Roman Mkrtchian <mkrtchia@gmail.com>
2021-01-30 20:06:05 +0100

    comment to understand better next rewrites config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,9 @@ const target = process.env.NEXT_PUBLIC_PROXY_HOST || "http://localhost:8000";
 
 module.exports = {
   async rewrites() {
+    // Redirects only client side calls to the correct backend address
+    // for server side calls it needs to be done where the calls are.
+    // (behavior to be checked regularly as next.js evolves)
     return [
       {
         source: "/api/:path*",
