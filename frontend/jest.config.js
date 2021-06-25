25404ed39ddb068a046e218d7067e596edfad3ab
Roman Mkrtchian <mkrtchia@gmail.com>
2021-06-25 23:59:30 +0200

    maint: update jest and babel-jest with correct config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "<rootDir>/node_modules/",
     "<rootDir>/out/",
   ],
+  "testEnvironment" : "jsdom",
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
