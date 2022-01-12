09681017198a89e2377d7261cca909148c398c8f
paoli-a <64416011+paoli-a@users.noreply.github.com>
2022-01-12 16:55:32 +0100

    style: transform mobile burger menu in modal (#727)

--- frontend/components/MyNextRecipe/Menu.test.tsx
+++ frontend/components/MyNextRecipe/Menu.test.tsx
@@ -6,7 +6,12 @@ import Menu from "./Menu";
 const useRouter = jest.spyOn(NextRouter, "useRouter");
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-useRouter.mockReturnValue({ pathname: "/" });
+useRouter.mockReturnValue({
+  pathname: "/",
+  prefetch: jest.fn(
+    () => new Promise<void>((resolve) => resolve())
+  ),
+});
 
 describe("Burger menu", () => {
   it(`sets the focus on the close icon when clicking on the burger icon and
@@ -20,15 +25,15 @@ describe("Burger menu", () => {
   });
 
   it(`sets correctly aria-expanded property on the burger and close buttons
-    when opening and closing the menu`, () => {
-    const { getByLabelText, getByRole } = render(<Menu />);
+    when opening and closing the menu`, async () => {
+    const { getByLabelText, getByRole, findByRole } = render(<Menu />);
     fireEvent.click(getByLabelText(/Ouvrir le menu/));
     expect(
       getByRole("button", { name: /Fermer le menu/, expanded: true })
     ).toBeInTheDocument();
     fireEvent.click(getByLabelText(/Fermer le menu/));
     expect(
-      getByRole("button", { name: /Ouvrir le menu/, expanded: false })
+      await findByRole("button", { name: /Ouvrir le menu/, expanded: false })
     ).toBeInTheDocument();
   });
 });
