6dd0533c887605237d27336d699d86d9a6a6491d
paoli-a <64416011+paoli-a@users.noreply.github.com>
2022-02-11 19:34:03 +0100

    style: mobile design of fridge ingredients form (#781)

--- frontend/end_to_end/tests/fridgeIngredients_test.ts
+++ frontend/end_to_end/tests/fridgeIngredients_test.ts
@@ -29,25 +29,25 @@ Scenario("See existing fridge ingredient", ({ I }) => {
 
 Scenario("Add new fridge ingredient", async ({ I }) => {
   I.amOnPage("/");
-  I.fillField("Nom de l'ingrédient :", "herbes fraiches");
-  I.fillField("Quantité :", "1");
+  I.fillField("Nom de l'ingrédient", "herbes fraiches");
+  I.fillField("Quantité", "1");
   I.selectOption("Unité", "pièce(s)");
-  I.fillField("Date de péremption :", "10102030");
+  I.fillField("Date de péremption", "10102030");
   I.click("Ajouter");
-  await within(".fridge-ingredients__list", () => {
+  await within(".fridgeIngredientsList", () => {
     I.see("échalotte");
     I.see("herbes fraiches");
   });
-  I.click(locate("img").withAttr({ alt: "Supprimer" }));
-  I.click(locate("img").withAttr({ alt: "Supprimer" }));
+  I.click("Supprimer l'ingrédient");
+  I.click("Supprimer l'ingrédient");
 });
 
 Scenario("Remove fridge ingredient", ({ I }) => {
   I.amOnPage("/");
   I.see("échalotte");
   I.click(
-    locate("img")
-      .withAttr({ alt: "Supprimer" })
+    locate("button")
+      .withAttr({ "aria-label": "Supprimer l'ingrédient" })
       .inside(locate("li").withChild("h3").withText("échalotte"))
   );
   I.dontSee("échalotte");
@@ -58,39 +58,39 @@ Scenario(
 still there`,
   async ({ I }) => {
     I.amOnPage("/");
-    I.fillField("Nom de l'ingrédient :", "herbes fraiches");
-    I.fillField("Quantité :", "1");
+    I.fillField("Nom de l'ingrédient", "herbes fraiches");
+    I.fillField("Quantité", "1");
     I.selectOption("Unité", "pièce(s)");
-    I.fillField("Date de péremption :", "10102030");
+    I.fillField("Date de péremption", "10102030");
     I.click("Ajouter");
     I.click("Catalogue recettes");
     I.click("Recettes possibles");
-    await within(".fridge-ingredients__list", () => {
+    await within(".fridgeIngredientsList", () => {
       I.see("échalotte");
       I.see("herbes fraiches");
     });
-    I.click(locate("img").withAttr({ alt: "Supprimer" }));
-    I.click(locate("img").withAttr({ alt: "Supprimer" }));
+    I.click("Supprimer l'ingrédient");
+    I.click("Supprimer l'ingrédient");
   }
 );
 
 Scenario("Edit existing fridge ingredient", async ({ I }) => {
   I.amOnPage("/");
   I.click(
-    locate("img")
-      .withAttr({ alt: "Modifier" })
+    locate("button")
+      .withAttr({ "aria-label": "Modifier l'ingrédient" })
       .inside(locate("li").withChild("h3").withText("échalotte"))
   );
   I.click(
-    locate("img")
-      .withAttr({ alt: "Modifier" })
+    locate("button")
+      .withAttr({ "aria-label": "Modifier l'ingrédient" })
       .inside(locate("li").withChild("h3").withText("citron vert"))
   );
-  I.fillField("Quantité :", "6");
+  I.fillField("Quantité", "6");
   I.selectOption("Unité", "g");
-  I.fillField("Date de péremption :", "10102033");
+  I.fillField("Date de péremption", "10102033");
   I.click("Modifier");
-  await within(".fridge-ingredients__list", () => {
+  await within(".fridgeIngredientsList", () => {
     I.see("citron vert");
     I.see("6");
     I.see("g");
