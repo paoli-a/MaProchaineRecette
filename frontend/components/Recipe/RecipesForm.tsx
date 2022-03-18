26492f9b4b4e5cc8ffa4584909a68d20896af469
paoli-a <64416011+paoli-a@users.noreply.github.com>
2022-03-18 11:18:51 +0100

    refactor: extract recipeIngredients fields from recipeForm (#842)

--- frontend/components/Recipe/RecipesForm.tsx
+++ frontend/components/Recipe/RecipesForm.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect } from "react";
-import { useFieldArray, useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import {
   CatalogRecipe,
   FridgeRecipe,
   RecipeIngredient,
-  SuggestionElement,
 } from "../../constants/types";
-import { useCatalogIngredients, useCategories, useUnits } from "../../hooks";
-import InputSuggestions from "../InputSuggestions/InputSuggestions";
+import { useCategories } from "../../hooks";
+import RecipeIngredientForm from "./RecipeIngredientForm";
 
 type FormInputs = {
   recipeTitle: string;
@@ -33,6 +32,11 @@ function RecipesForm<T extends FridgeRecipe | CatalogRecipe>({
   recipeToEdit,
   resetRecipeToEdit,
 }: RecipesFormProps<T>): JSX.Element {
+  const methods = useForm<FormInputs>({
+    defaultValues: {
+      recipeIngredients: [{}],
+    },
+  });
   const {
     register,
     handleSubmit,
@@ -42,21 +46,9 @@ function RecipesForm<T extends FridgeRecipe | CatalogRecipe>({
     getValues,
     setValue,
     setFocus,
-    control,
-  } = useForm<FormInputs>({
-    defaultValues: {
-      recipeIngredients: [{}],
-    },
-  });
+  } = methods;
 
-  const { fields, append, remove, replace } = useFieldArray({
-    control,
-    name: "recipeIngredients",
-  });
-
-  const { catalogIngredients } = useCatalogIngredients();
   const { categories } = useCategories();
-  const { units } = useUnits();
 
   useEffect(() => {
     if (recipeToEdit) {
@@ -74,9 +66,8 @@ function RecipesForm<T extends FridgeRecipe | CatalogRecipe>({
       setValue(`categories`, categoriesToEdit);
       setValue("recipeTime", recipeToEdit.duration);
       setValue("recipeDescription", recipeToEdit.description);
-      replace(recipeToEdit.ingredients);
     }
-  }, [recipeToEdit, setFocus, categories, setValue, replace]);
+  }, [recipeToEdit, setFocus, categories, setValue]);
 
   const onSubmitForm = (data: FormInputs) => {
     const clearedIngredients = data.recipeIngredients.filter(
@@ -90,87 +81,6 @@ function RecipesForm<T extends FridgeRecipe | CatalogRecipe>({
     onSubmitRecipe(clearedData);
     reset();
   };
-
-  function getCleanedIngredients() {
-    const ingredients = watch(`recipeIngredients`);
-    return ingredients.map(function eliminateEmptyIngredient(ingredient) {
-      if (!ingredient.ingredient && !ingredient.amount && !ingredient.unit) {
-        return null;
-      } else return ingredient;
-    });
-  }
-
-  function validateIngredients(index: number) {
-    const ingredients = getCleanedIngredients();
-    const noIngredientsFilledAtAll = ingredients.every(
-      (ingredient) => ingredient === null
-    );
-    if (noIngredientsFilledAtAll) {
-      return "Il faut au moins un ingrédient dans la recette";
-    } else if (ingredients[index] === null) {
-      return true;
-    }
-
-    const ingredientNames = ingredients
-      .map((recipeIngredients) => recipeIngredients?.ingredient)
-      .slice();
-    const ingredient = ingredientNames[index];
-
-    let authorized = false;
-    for (const ingredientPossible of catalogIngredients) {
-      if (ingredientPossible.name === ingredient) {
-        authorized = true;
-        break;
-      }
-    }
-    if (!authorized) {
-      return "Cet ingrédient n'existe pas dans le catalogue d'ingrédients. Vous pouvez l'y ajouter ";
-    }
-
-    ingredientNames.splice(index, 1);
-    if (ingredientNames.includes(ingredient)) {
-      return "Vous ne pouvez pas ajouter plusieurs fois le même ingrédient";
-    }
-
-    return true;
-  }
-
-  function validateAmounts(index: number) {
-    const amount = watch(`recipeIngredients.${index}.amount`);
-    const ingredients = getCleanedIngredients();
-    const noIngredientsFilledAtAll = ingredients.every(
-      (ingredient) => ingredient === null
-    );
-    if (noIngredientsFilledAtAll) {
-      return false;
-    } else if (ingredients[index] === null) {
-      return true;
-    }
-    if (amount === "") {
-      return "Ce champ est obligatoire";
-    }
-
-    if (parseFloat(amount) <= 0) {
-      return "La quantité doit être supérieure à 0";
-    } else return true;
-  }
-
-  function validateUnits(index: number) {
-    const unit = watch(`recipeIngredients.${index}.unit`);
-    const ingredients = getCleanedIngredients();
-    const noIngredientsFilledAtAll = ingredients.every(
-      (ingredient) => ingredient === null
-    );
-    if (noIngredientsFilledAtAll) {
-      return false;
-    } else if (ingredients[index] === null) {
-      return true;
-    }
-    if (unit === "") {
-      return "Ce champ est obligatoire";
-    }
-    return true;
-  }
 
   const handleCancelClick = () => {
     resetRecipeToEdit && resetRecipeToEdit();
@@ -197,248 +107,136 @@ function RecipesForm<T extends FridgeRecipe | CatalogRecipe>({
   };
 
   return (
-    <form className="form form-recipe" onSubmit={handleSubmit(onSubmitForm)}>
-      <fieldset>
-        <legend>
-          {recipeToEdit
-            ? "Modifier une recette de mon catalogue :"
-            : "Ajouter une recette dans mon catalogue :"}
-        </legend>
-        <div className="form__paragraph">
-          <label className="form__label" htmlFor="recipeTitle">
-            {" "}
-            Titre de la recette :{" "}
-          </label>
-          <div className="container-error">
-            <input
+    <FormProvider {...methods}>
+      <form className="form form-recipe" onSubmit={handleSubmit(onSubmitForm)}>
+        <fieldset>
+          <legend>
+            {recipeToEdit
+              ? "Modifier une recette de mon catalogue :"
+              : "Ajouter une recette dans mon catalogue :"}
+          </legend>
+          <div className="form__paragraph">
+            <label className="form__label" htmlFor="recipeTitle">
+              {" "}
+              Titre de la recette :{" "}
+            </label>
+            <div className="container-error">
+              <input
+                className={
+                  errors.recipeTitle ? "form__input field-error" : "form__input"
+                }
+                type="text"
+                id="recipeTitle"
+                defaultValue=""
+                {...register("recipeTitle", { required: true })}
+                aria-invalid={errors.recipeTitle ? "true" : "false"}
+                aria-required="true"
+              />
+              {errors.recipeTitle && (
+                <p className="form__error-message" role="alert">
+                  Ce champ est obligatoire
+                </p>
+              )}
+            </div>
+          </div>
+          <div className="form__checkbox-container">
+            Catégories :
+            <ul>
+              {categories.map((category: string, index: number) => {
+                return (
+                  <li key={category}>
+                    <input
+                      type="checkbox"
+                      value={category}
+                      aria-label={category}
+                      {...register(`categories.${index}`, {
+                        validate: validateCategories,
+                      })}
+                      aria-invalid={errors.categories ? "true" : "false"}
+                      id={`category-${index}`}
+                    />
+                    <label htmlFor={`category-${index}`}>{category}</label>
+                  </li>
+                );
+              })}
+            </ul>
+            {errors.categories && (
+              <p className="form__error-message" role="alert">
+                Au moins une catégorie doit être sélectionnée
+              </p>
+            )}
+          </div>
+          <div className="form__paragraph">
+            <label className="form__label" htmlFor="recipeTime">
+              {" "}
+              Temps total de la recette :{" "}
+            </label>
+            <div className="container-error">
+              <input
+                className={
+                  errors.recipeTime ? "form__input field-error" : "form__input"
+                }
+                type="time"
+                id="recipeTime"
+                {...register("recipeTime", {
+                  required: "Ce champ est obligatoire",
+                  validate: validateTime,
+                })}
+                aria-invalid={errors.recipeTime ? "true" : "false"}
+                aria-required="true"
+              />
+              {errors.recipeTime && (
+                <p className="form__error-message" role="alert">
+                  {errors.recipeTime.message}
+                </p>
+              )}
+            </div>
+          </div>
+          <RecipeIngredientForm recipeToEdit={recipeToEdit} />
+          <div className="form__textarea-container">
+            <label className="form__label" htmlFor="recipeDescription">
+              Corps de la recette :{" "}
+            </label>
+            <textarea
               className={
-                errors.recipeTitle ? "form__input field-error" : "form__input"
+                errors.recipeTime
+                  ? "form__textarea field-error"
+                  : "form__textarea"
               }
-              type="text"
-              id="recipeTitle"
-              defaultValue=""
-              {...register("recipeTitle", { required: true })}
-              aria-invalid={errors.recipeTitle ? "true" : "false"}
+              id="recipeDescription"
+              spellCheck="true"
+              {...register("recipeDescription", { required: true })}
+              aria-invalid={errors.recipeDescription ? "true" : "false"}
               aria-required="true"
-            />
-            {errors.recipeTitle && (
+            ></textarea>
+            {errors.recipeDescription && (
               <p className="form__error-message" role="alert">
                 Ce champ est obligatoire
               </p>
             )}
           </div>
-        </div>
-        <div className="form__checkbox-container">
-          Catégories :
-          <ul>
-            {categories.map((category: string, index: number) => {
-              return (
-                <li key={category}>
-                  <input
-                    type="checkbox"
-                    value={category}
-                    aria-label={category}
-                    {...register(`categories.${index}`, {
-                      validate: validateCategories,
-                    })}
-                    aria-invalid={errors.categories ? "true" : "false"}
-                    id={`category-${index}`}
-                  />
-                  <label htmlFor={`category-${index}`}>{category}</label>
-                </li>
-              );
-            })}
-          </ul>
-          {errors.categories && (
-            <p className="form__error-message" role="alert">
-              Au moins une catégorie doit être sélectionnée
-            </p>
-          )}
-        </div>
-        <div className="form__paragraph">
-          <label className="form__label" htmlFor="recipeTime">
-            {" "}
-            Temps total de la recette :{" "}
-          </label>
-          <div className="container-error">
+          <p className="form__paragraph">
             <input
-              className={
-                errors.recipeTime ? "form__input field-error" : "form__input"
+              className="button form__submit"
+              type="submit"
+              aria-label={
+                recipeToEdit ? "Modifier la recette" : "Ajouter la recette"
               }
-              type="time"
-              id="recipeTime"
-              {...register("recipeTime", {
-                required: "Ce champ est obligatoire",
-                validate: validateTime,
-              })}
-              aria-invalid={errors.recipeTime ? "true" : "false"}
-              aria-required="true"
+              value={recipeToEdit ? "Modifier" : "Ajouter"}
             />
-            {errors.recipeTime && (
-              <p className="form__error-message" role="alert">
-                {errors.recipeTime.message}
-              </p>
+            {recipeToEdit && (
+              <button
+                className="button form__cancel"
+                type="reset"
+                onClick={() => handleCancelClick()}
+              >
+                Annuler
+              </button>
             )}
-          </div>
-        </div>
-        <fieldset className="form form-ingredient-recipe">
-          <legend>Ingrédients :</legend>
-          <div className="form__paragraph">
-            <ul>
-              {fields.map((item, index) => {
-                return (
-                  <li key={item.id}>
-                    <label
-                      className="form__label"
-                      htmlFor={`ingredient${index}`}
-                    >
-                      Nom
-                    </label>
-                    <InputSuggestions
-                      className={
-                        errors[`recipeIngredients`] &&
-                        errors[`recipeIngredients`][index] &&
-                        errors[`recipeIngredients`][index].ingredient
-                          ? "form__input field-error"
-                          : "form__input"
-                      }
-                      elements={catalogIngredients}
-                      id={`ingredient${index}`}
-                      getElementText={(ingredient: SuggestionElement) =>
-                        ingredient.name
-                      }
-                      {...register(`recipeIngredients.${index}.ingredient`, {
-                        validate: () => validateIngredients(index),
-                      })}
-                      type="text"
-                      aria-required="true"
-                    />
-                    {errors.recipeIngredients &&
-                      errors[`recipeIngredients`][index] &&
-                      errors[`recipeIngredients`][index].ingredient && (
-                        <p className="form__error-message" role="alert">
-                          {
-                            errors[`recipeIngredients`][index].ingredient
-                              ?.message
-                          }
-                        </p>
-                      )}
-                    <label
-                      className="form__label"
-                      htmlFor={`ingredientAmount${index}`}
-                    >
-                      Quantité nécessaire
-                    </label>
-                    <span className="form__combined-container">
-                      <input
-                        className="form__combined-input"
-                        type="number"
-                        id={`ingredientAmount${index}`}
-                        min="0"
-                        step=".01"
-                        {...register(`recipeIngredients.${index}.amount`, {
-                          validate: () => validateAmounts(index),
-                        })}
-                        aria-required="true"
-                      />
-                      {errors.recipeIngredients &&
-                        errors[`recipeIngredients`][index] &&
-                        errors[`recipeIngredients`][index].amount && (
-                          <p className="form__error-message" role="alert">
-                            {errors[`recipeIngredients`][index].amount?.message}
-                          </p>
-                        )}
-                      <select
-                        className="form__combined-select"
-                        aria-label="Unité"
-                        {...register(`recipeIngredients.${index}.unit`, {
-                          validate: () => validateUnits(index),
-                        })}
-                        aria-required="true"
-                      >
-                        <option value="">...</option>
-                        {units.map((unit: string) => {
-                          return (
-                            <option value={unit} key={unit}>
-                              {unit}
-                            </option>
-                          );
-                        })}
-                      </select>
-                      {errors.recipeIngredients &&
-                        errors[`recipeIngredients`][index] &&
-                        errors[`recipeIngredients`][index].unit && (
-                          <p className="form__error-message" role="alert">
-                            {errors[`recipeIngredients`][index].unit?.message}
-                          </p>
-                        )}
-                    </span>
-                    {index === fields.length - 1 ? (
-                      <button
-                        aria-label="Ingredient supplémentaire (plus)"
-                        type="button"
-                        onClick={() => append({})}
-                      >
-                        +
-                      </button>
-                    ) : (
-                      <button
-                        aria-label="Supprimer cet ingredient (moins)"
-                        type="button"
-                        onClick={() => remove(index)}
-                      >
-                        -
-                      </button>
-                    )}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+          </p>
         </fieldset>
-        <div className="form__textarea-container">
-          <label className="form__label" htmlFor="recipeDescription">
-            Corps de la recette :{" "}
-          </label>
-          <textarea
-            className={
-              errors.recipeTime
-                ? "form__textarea field-error"
-                : "form__textarea"
-            }
-            id="recipeDescription"
-            spellCheck="true"
-            {...register("recipeDescription", { required: true })}
-            aria-invalid={errors.recipeDescription ? "true" : "false"}
-            aria-required="true"
-          ></textarea>
-          {errors.recipeDescription && (
-            <p className="form__error-message" role="alert">
-              Ce champ est obligatoire
-            </p>
-          )}
-        </div>
-        <p className="form__paragraph">
-          <input
-            className="button form__submit"
-            type="submit"
-            aria-label={
-              recipeToEdit ? "Modifier la recette" : "Ajouter la recette"
-            }
-            value={recipeToEdit ? "Modifier" : "Ajouter"}
-          />
-          {recipeToEdit && (
-            <button
-              className="button form__cancel"
-              type="reset"
-              onClick={() => handleCancelClick()}
-            >
-              Annuler
-            </button>
-          )}
-        </p>
-      </fieldset>
-    </form>
+      </form>
+    </FormProvider>
   );
 }
 
